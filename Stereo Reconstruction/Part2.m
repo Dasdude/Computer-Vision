37ea2c73beb6c80fb0c96306843deed180714878
ehsan emad <e_emad@knights.ucf.edu>
2016-05-04 16:45:49 -0400

    Part 2 Fixed

--- Stereo Reconstruction/Part2.m
+++ Stereo Reconstruction/Part2.m
@@ -2,10 +2,11 @@ clear
 close all
 M = load('./Data/measurement_matrix.txt');
 %% 
-[O1 sig O2] = svd(M);
-F = size(M,1)/2;
-Rhat = O1(:,1:3)*sqrt(sig(1:3,1:3));
-Shat = sqrt(sig(1:3,1:3))*O2(1:3,:);
+    [O1 sig O2] = svd(M);
+    O2=O2';
+    F = size(M,1)/2;
+    Rhat = O1(:,1:3)*sqrt(sig(1:3,1:3));
+    Shat = sqrt(sig(1:3,1:3))*O2(1:3,:);
 %% Calculating Q in the quadratic system with constraints
 %   R
     i = Rhat(1:F,:);
@@ -15,24 +16,21 @@ Shat = sqrt(sig(1:3,1:3))*O2(1:3,:);
     G3 = [j(:,1).*i(:,1),(i(:,1).*j(:,2))+(i(:,2).*j(:,1)),(i(:,1).*j(:,3))+(j(:,1).*i(:,3)),j(:,2).*i(:,2),(i(:,2).*j(:,3))+(j(:,2).*i(:,3)),j(:,3).*i(:,3)];
     G = [G1;G2;G3];
     c = [ones(1,2*F),zeros(1,F)]';
-    I = pinv(G)*c;
+    I = G\c;
     L = [I(1),I(2),I(3);I(2),I(4),I(5);I(3),I(5),I(6)];
 % Enforcing positive definite
-    eps = 10^(-5);
+    eps = 10^(-100);
     [U,S,V] = svd(L);
     index = find(S<0);
-    S(index) = eps;
+    S(index) =eps;
     Q = U*sqrt(S);
 %% Computing Rotation and Shape Matrix
     k = cross(i(1,:)',j(1,:)');
     Rz = [i(1,:)'/norm(i(1,:)),j(1,:)'/norm(j(1,:)),k/norm(k)];
     R = Rhat*Q;
-%     R = R*Rz;
-    S = inv(Q)*Shat;
-%     S = Rz'*S;
+    S = Q\Shat;
     figure;
     plot3(S(1,:),S(2,:),S(3,:),'.b')
     axis equal
-%     R = 
 
 
